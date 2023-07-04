f517fab2f8a5617eaf77d17976c12ead709ebf7f
ryr0121 <ryr0121@gmail.com>
2023-07-04 15:19:50 +0900

    구글맵을 이용한 출근 확인 앱 구현

---
+++ check_go_work/android/app/src/main/kotlin/com/example/check_go_work/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.check_go_work
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ check_go_work/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:check_go_work/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    )
+  );
+}
